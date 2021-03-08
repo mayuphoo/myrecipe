5996d22a401fac8b6e686c753d5c7a7268e4abe2
mayuphoo <76111639+mayuphoo@users.noreply.github.com>
2021-03-09 01:30:17 +0900

    その他

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  # extend ActiveHash::Associations::ActiveRecordExtensions
+  # belongs_to :category
+  
   belongs_to :user
   has_one_attached :image
   has_many :comments, dependent: :destroy
