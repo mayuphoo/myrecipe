60bdcfb88d3b7c277e73d6b31ec80e0e34ae0e61
mayuphoo <76111639+mayuphoo@users.noreply.github.com>
2021-03-09 22:06:53 +0900

    ユーザー管理機能のテストコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまく行くとき' do
+      it '全ての項目が存在すれば、登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it 'nameが空では登録できない' do
+        @user.name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it '重複したemailが存在する場合、登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'emailに@が含まれていないと、登録できない' do
+        @user.email = 'test.co.jp'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank",
+                                                      "Password confirmation doesn't match Password")
+      end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = 'aa11'
+        @user.password_confirmation = 'aa11'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it 'passwordは存在しても、password_confilmationが存在していないと登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'password,password_confirmationが一致していないと登録できない' do
+        @user.password = 'cccc9999'
+        @user.password_confirmation = 'cccc8888'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+    end
+  end
+end
