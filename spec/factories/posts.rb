49f3004507bd91677f13b5dc94e9df9dd642fe9f
mayuphoo <76111639+mayuphoo@users.noreply.github.com>
2021-03-09 22:07:09 +0900

    投稿機能のテストコード

---
+++ spec/factories/posts.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :post do
+    title          { 'りんご' }
+    body { Faker::Lorem.sentence }
+    user_id { Faker::Number.between(300, 9_999_999) }
+    category_id { '3' }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
