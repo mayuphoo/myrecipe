cd266e885bae48a8da83f62425bee5f8720863b1
mayuphoo <76111639+mayuphoo@users.noreply.github.com>
2021-03-11 00:54:13 +0900

    その他

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,17 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    comment = @post.comments.find(params[:id])
+    if current_user.id == comment.user_id
+      comment.destroy
+    redirect_to post_path(params[:post_id])
+    else
+      render "posts/show"
+    end
+  end
+
 private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
